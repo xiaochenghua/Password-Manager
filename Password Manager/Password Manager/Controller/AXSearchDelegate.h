2f3bada8ae02158d818140524da98a88c2cf933c
xiaochenghua <xiaochenghua@xiaoniu66.com>
2017-08-02 16:49:45 +0800

    commit search delegate

---
+++ Password Manager/Password Manager/Controller/AXSearchDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AXSearchDelegate.h
+//  Password Manager
+//
+//  Created by arnoldxiao on 2017/1/3.
+//  Copyright © 2017年 arnoldxiao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AXSearchDelegate : NSObject
+
+@end
---
+++ Password Manager/Password Manager/Controller/AXSearchDelegate.m
@@ -0,0 +1,13 @@
+//
+//  AXSearchDelegate.m
+//  Password Manager
+//
+//  Created by arnoldxiao on 2017/1/3.
+//  Copyright © 2017年 arnoldxiao. All rights reserved.
+//
+
+#import "AXSearchDelegate.h"
+
+@implementation AXSearchDelegate
+
+@end
